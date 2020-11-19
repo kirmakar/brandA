af74ba7c4a669ba69dab281e35d5040010556b58
Kirill Makarikhin <Kirill_Makarikhin@epam.com>
2020-11-19 22:13:39 +0300

    test cookie setting

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3000;
+
+app.use(cookieParser());
+
+app.use(function (req, res, next) {
+  const cookie1 = req.cookies.cookieName1;
+  const cookie2 = req.cookies.cookieName2;
+  
+  const getRandomNumber = () => { 
+      let randomNumber = Math.random().toString(); 
+      return randomNumber.substring(2, randomNumber.length)
+  };
+   
+  if (cookie1 === undefined) {
+    res.cookie('cookieName1', getRandomNumber(), { maxAge: 900000, httpOnly: true });
+  } 
+  if (cookie1 === undefined) {
+    res.cookie('cookieName2', getRandomNumber(), { maxAge: 900000, httpOnly: true });
+  } 
+
+  next(); 
+});
 
 app.use(express.static("src"));
 
